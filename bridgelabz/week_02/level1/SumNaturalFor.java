ca13a71c0a68f4467d35c69a7420c187adc541b3
pratyushraghuv <raghuvanshipratyush01@gmail.com>
2025-09-02 10:35:12 +0530

    added week 2

---
+++ bridgelabz/week_02/level1/SumNaturalFor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumNaturalFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n > 0){
+            int formula = n*(n+1)/2;
+            int sum = 0;
+            for(int i = 1; i <= n; i++) sum += i;
+            System.out.println("Formula result = " + formula + ", For loop result = " + sum);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
